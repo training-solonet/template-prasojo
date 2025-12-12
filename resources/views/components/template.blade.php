4afd0f6ddcfd5daac039fface07462794a6ee74f
bimopandapotan <bimosatp522@gmail.com>
2025-12-12 15:37:44 +0700

    menyelesaikan tab permintaan produksi dan memulai tab daftar spp

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<link rel="stylesheet" href="{{ asset('style.css') }}">
-	@vite('resources/css/app.css')
 	<title>Prasojo App</title>
 </head>
 <body>
--- resources/views/produksi/permintaanproduksi.blade.php
+++ resources/views/produksi/permintaanproduksi.blade.php
@@ -15,40 +15,53 @@
                     Data permintaan produksi yang siap dibuatkan SPP
                 </div>
             </div>
-            <div>
-                <button class="btn btn-primary">Tambah Supplier</button>
-            </div>
         </div>
         <div style="background:var(--card); margin-top:20px; padding:8px; border-radius:10px; border:1px solid var(--glass);">
+            <input id="search" name="search" placeholder="Cari permintaan..." class=" search-bar form-group">
+            <br>
             <table>
                 <thead>
                     <tr>
-                        <th>Nama</th>
-                        <th>Kontak</th>
-                        <th>Top (hari)</th>
-                        <th>Alamat</th>
+                        <th>No</th>
+                        <th>No Permintaan</th>
+                        <th>Produk</th>
+                        <th>Jumlah</th>
+                        <th>Marketing</th>
+                        <th>Status</th>
+                        <th>Tanggal</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Sumber Batik</td>
-                        <td>08123456789</td>
-                        <td>30</td>
-                        <td>Jl. Batik No. 12</td>
-                        <td>
-                            <button class="btn btn-ghost edit-supplier" onclick="openModalEdit()">Edit</button>
-                            <button class="btn btn-danger delete-supplier" onclick="deleteRow(this)">Delete</button>
+                        <td>1</td>
+                        <td>REQ-2025-003</td>
+                        <td>Kemeja Batik</td>
+                        <td>200</td>
+                        <td>Dina</td>
+                        <td><span class="info-warning">Menunggu SPP</span></td>
+                        <td>25 Okt 2025</td>
+                        <td><button 
+                                class="btn-spp" 
+                                data-index="0" 
+                                data-no="REQ-2025-003" 
+                                data-produk="Kemeja Batik" 
+                                data-jumlah="200"
+                                onclick="openBuatSPP(this)">
+                                Buat SPP
+                            </button>
                         </td>
                     </tr>
                     <tr>
-                        <td>CV. Kain Lurik</td>
-                        <td>01823854923</td>
-                        <td>14</td>
-                        <td>Desa Tenun</td>
+                        <td>2</td>
+                        <td>REQ-2025-002</td>
+                        <td>Dress Lurik</td>
+                        <td>120</td>
+                        <td>Rina</td>
+                        <td><span class="info-success">SPP Terbit</span></td>
+                        <td>26 Okt 2025</td>
                         <td>
-                            <button class="btn btn-ghost edit-supplier" onclick="openModalEdit()">Edit</button>
-                            <button class="btn btn-danger delete-supplier" onclick="deleteRow(this)">Delete</button>
+                            -
                         </td>
                     </tr>
                 </tbody>
@@ -56,3 +69,71 @@
         </div>
     </div>
 </x-template>
+
+<div id="modalBuatSPP" class="modal-overlay" style="display:none;">
+    <div class="modal-box">
+        <div class="modal-header">
+            <h3>Buat Surat Perintah Produksi (SPP)</h3>
+            <button class="btn btn-ghost" onclick="closeBuatSPP()" style="font-size: 20px">×</button>
+        </div>
+        <div class="modal-body">
+            <div style="margin-top: 3px">
+                <label for='noSPP' style="font-size: 12px; color: #17233A;">No SPP</label>
+                <input class="form-group" type='text' name='noSPP' id='noSPP' readonly>
+            </div>
+            <div style="margin-top: 3px">
+                <label for='noPermintaan' style="font-size: 12px; color: #17233A;">No Permintaan</label>
+                <input class="form-group" type='text' name='noPermintaan' id='noPermintaan' readonly>
+            </div>
+            <div style="margin-top: 3px">
+                <label for='produk' style="font-size: 12px; color: #17233A;" readonly>Produk</label>
+                <input class="form-group" type='text' name='produk' id='produk'>
+            </div>
+            <div style="margin-top: 3px">
+                <label for='jumlahProduksi' style="font-size: 12px; color: #17233A;">Jumlah Produksi</label>
+                <input class="form-group" type='number' name='jumlahProduksi' id='jumlahProduksi'>
+            </div>
+            <div style="margin-top: 3px">
+                <label for='keterangan' style="font-size: 12px; color: #17233A;">Keterangan</label>
+                <textarea class="form-group" type='text' name='keterangan' id='keterangan'></textarea>
+            </div>
+        </div>
+        <br>
+        <div class="modal-footer">
+            <button class="btn btn-primary" onclick="simpanBuatSPP()">Simpan</button>
+        </div>
+    </div>
+</div>
+
+<script>
+    const modalSPP = document.getElementById('modalBuatSPP');
+    let currentButton = null;
+
+    function openBuatSPP(data){
+        currentButton = data;
+        modalSPP.style.display = 'flex';
+        document.getElementById('noPermintaan').value = data.dataset.no;
+        document.getElementById('produk').value = data.dataset.produk;
+        document.getElementById('jumlahProduksi').value = data.dataset.jumlah;
+        document.getElementById('noSPP').value = 'SPP-2025-339';
+    }
+
+    function closeBuatSPP(data){
+        modalSPP.style.display = 'none';
+    }
+
+    function simpanBuatSPP(){
+        alert('SPP SPP-2025-429 untuk permintaan REQ-2025-003 berhasil disimpan!');
+        modalSPP.style.display = 'none';
+
+        let row = currentButton.closest("tr");
+
+        let status = row.querySelector("td:nth-child(6) span")
+        status.textContent = "SPP Terbit";
+        status.classList.remove("info-warning");
+        status.classList.add("info-success");
+
+        let button = row.querySelector("td:nth-child(8)");
+        button.innerHTML = '-';
+    }
+</script>
