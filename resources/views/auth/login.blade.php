8ab9687f87eb52dc4999ad155f1a1bce9dd4c914
bimopandapotan <bimosatp522@gmail.com>
2025-12-29 13:50:44 +0700

    Memperbarui tampilan login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login | Lurik Prasojo Pedan</title>
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    />
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('login.css') }}">
 </head>
 <body>
-    <form method="POST" action="{{ route('login.index') }}">
-        @csrf
-        <input name="username" type="username">
-        <input name="password" type="password">
-        <button>Login</button>
-    </form>
+
+<div class="login-wrapper">
+    <div class="login-card">
+
+        <div class="logo">
+            <img src="https://down-id.img.susercontent.com/file/8d4b3cadfed0c9af76df1cb48da33fb2_tn" alt="Logo">
+        </div>
+
+        <h1>Lurik Prasojo Pedan</h1>
+        <h2>Login</h2>
+
+        <form method="POST" action="{{ route('login.index') }}">
+            @csrf
+
+            <div class="input-group">
+                <span class="icon"><i class="fa-solid fa-user"></i></span>
+                <input type="text" name="username" placeholder="Username" required>
+            </div>
+
+            <div class="input-group">
+                <span class="icon"><i class="fa-solid fa-lock"></i></span>
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+
+            <button type="submit" class="btn-login">Masuk</button>
+        </form>
+
+        <p class="copyright">
+            © 2025 Lurik Prasojo Pedan — All Rights Reserved
+        </p>
+
+    </div>
+</div>
+
 </body>
 </html>
